8472aa44ca6f094e1f12dc8981e577c8d997c3bf
marcochiesa <marco_chiesa@hotmail.com>
2014-10-19 00:49:28 -0500

    Add skeleton code to get app running.

---
+++ src/main/java/soph/collab/MainController.java
@@ -0,0 +1,14 @@
+package soph.collab;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from SoPH Collab!";
+    }
+
+}
